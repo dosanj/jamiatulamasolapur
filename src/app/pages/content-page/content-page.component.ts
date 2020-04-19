ffb92c907531458934aa45bd12403bd443de175c
dosanj <javeddosani2011@gmail.com>
2020-04-19 20:57:16 +0530

    add home page

--- src/app/pages/content-page/content-page.component.ts
+++ src/app/pages/content-page/content-page.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { contentData, getContentData } from '../../shared/content-data';
 import { IContentData, ISideMenuItem } from '../../shared/shared-models';
 import { sideMenuDetails } from '../home-page/navigation-page-data';
-
+interface IHomePageItem {
+    text: string;
+    link?: string;
+    url?: string;
+    background?: string;
+}
 @Component({
     selector: 'app-content-page',
     templateUrl: './content-page.component.html',
@@ -14,12 +19,50 @@ import { sideMenuDetails } from '../home-page/navigation-page-data';
 export class ContentPageComponent implements OnInit {
     content$: Observable<IContentData>;
     contentData = getContentData();
-    constructor(private route: ActivatedRoute) {}
+    homePageItems: IHomePageItem[] = [
+        {
+            text: ' Cleanliness In Muslim Society ',
+            link: '/cleanliness',
+        },
+        {
+            text: 'Property Distribution ',
+            link: '/property-distribution',
+        },
+        { text: 'Corruption', link: '/corruption' },
+        {
+            text: 'Marriage in Islam',
+            link: '/marriage-age',
+        },
+        { text: 'Parenting Guidelines', link: '/upto-5-years' },
+        { text: 'Special Occasions', link: '/eid-ul-fitr' },
+        { text: 'Islamic History', link: '/muslim-in-freedom-of-india' },
+        {
+            text: 'Applying for Govt. Documents',
+            link: '/apply-for-govt.-documents',
+        },
+    ];
+    constructor(private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit() {
         this.content$ = this.route.params.pipe(
             tap(console.log),
             map((params: any) => this.contentData['/' + params.link])
         );
+        this.homePageItems.forEach((item: IHomePageItem) => {
+            item.background =
+                '#' +
+                (
+                    'd0dee' + ((Math.random() * (1 << 24)) | 0).toString(16)
+                ).slice(-6) +
+                '50';
+            console.log(item.background);
+        });
+    }
+    openLink(item: IHomePageItem) {
+        if (item.url) {
+            window.open(item.url, '_blank');
+        } else if (item.link) {
+            this.router.navigate([item.link]);
+        }
     }
 }
